72c53a3227f244a923bde12e4d6a1ea7d4b7a6f2
lbfrancisco <lucasbarroso318@gmail.com>
2024-05-30 22:08:16 -0300

    style: removing height calc break

--- src/layouts/default-layout/styles.ts
+++ src/layouts/default-layout/styles.ts
@@ -1,9 +1,8 @@
 import styled from 'styled-components'
 
 export const LayoutContainer = styled.div`
-  max-width: 75rem;
-  height: calc(100vh - 10rem);
-  margin: 5rem auto;
+  max-width: 70rem;
+  margin: 2.5rem auto;
   padding: 2.5rem;
 
   background: ${({ theme }) => theme['gray-800']};
